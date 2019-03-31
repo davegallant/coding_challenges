15f2647b4abd7587c6bea1edae129d65d1409720
davegallant <davegallant@gmail.com>
2019-03-31 07:28:43 -0400

    first recurring character.

---
+++ coding_challenges/first_recurring_character.py
@@ -0,0 +1,23 @@
+"""Given a string, get the first recurring character."""
+
+# Examples:
+# abcdea -> a
+# ddd -> d
+
+# Edges cases:
+# abc -> None
+# -> None
+
+
+def get_first_recurring_char(given_array):
+    occurences = set()
+    for char in given_array:
+        if char in occurences:
+            return char
+        occurences.add(char)
+
+
+assert get_first_recurring_char("abcdab") == "a"
+assert get_first_recurring_char("abc") == None
+assert get_first_recurring_char("") == None
+
