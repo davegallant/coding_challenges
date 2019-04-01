4b824d22f175bdf1bfe5f9d840d4971fa29249f0
davegallant <davegallant@gmail.com>
2019-03-31 21:52:18 -0400

    fix current max locaiton.

--- coding_challenges/k_unique_substrings.py
+++ coding_challenges/k_unique_substrings.py
@@ -23,13 +23,14 @@ def k_unique_substrings(given_string, k):
     for char in given_string:
         current_characters.add(char)
         window += char
+        # If the set contains more than k, cleanup
         if len(current_characters) > k:
             if len(window[:-1]) > current_max:
                 longest_substrings = []
                 longest_substrings.append(window[:-1])
+                current_max = len(window[:-1])
             elif len(window[:-1]) == current_max:
                 longest_substrings.append(window[:-1])
-            current_max = len(window[:-1])
             window_set = set()
             # Trim the window from the left
             for w in range(len(window)):
@@ -38,11 +39,18 @@ def k_unique_substrings(given_string, k):
                     current_characters.remove(window[w - 1])
                     window = window[w:]
                     break
+    # If the remaining window is the same
     if len(window) >= current_max:
+        longest_substrings.append(window)
+    # If the remaining window is the largest
+    if len(window) > current_max:
+        longest_substrings = []
         longest_substrings.append(window)
     return longest_substrings
 
 
 assert k_unique_substrings("aabbcc", k=1) == ["aa", "bb", "cc"]
+assert k_unique_substrings("aabbccc", k=1) == ["ccc"]
+assert k_unique_substrings("aaaabbccc", k=1) == ["aaaa"]
 assert k_unique_substrings("aabbcc", k=2) == ["aabb", "bbcc"]
 assert k_unique_substrings("aabbcc", k=3) == ["aabbcc"]
